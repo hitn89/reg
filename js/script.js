beb4fb88a140d4f6e6af90a0cbb7e947c9e8f70c
hitn89 <hitn89@gmail.com>
2017-02-11 13:27:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// проверка логина на длину, латинские символы и цифры, первая буква
 function loginScan() {
   var login = document.getElementById("login").value;
   var errLogin = "";
@@ -14,6 +15,7 @@ function loginScan() {
 document.getElementById('errLogin').innerHTML = errLogin;
 }
 
+// проверка почты регулярным выражением
 function mailScan() {
   var mail = document.getElementById("mail").value;
   var errMail = "";
@@ -25,4 +27,50 @@ function mailScan() {
   }
 
 document.getElementById('errMail').innerHTML = errMail;
+}
+
+
+// проверка имени
+function nameScan() {
+  var name = document.getElementById("name").value;
+  var errName = "";
+
+  var re = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
+
+  if(!re.test(name)){
+    errName += "Не корректно введено Имя";
+  }
+
+document.getElementById('errName').innerHTML = errName;
+}
+
+
+// проверка пароля
+function passwordScan() {
+
+  document.getElementById('errPasswordR').innerHTML = "Пароли не одинаковы";
+  var password = document.getElementById("password").value;
+  var errPassword = "";
+
+  var re = /^[\d\w]{3,10}$/;
+
+  if(!re.test(password)){
+    errPassword += "Не корректно введен пароль. Пароль должен содержать от 3х до 10 символов, и содержать только латинские символы и цифры";
+  }
+
+document.getElementById('errPassword').innerHTML = errPassword;
+}
+
+
+// сравниваем пароли
+function passwordRScan() {
+  var passwordR = document.getElementById("passwordR").value;
+  var password = document.getElementById("password").value;
+  var errPasswordR = "";
+
+  if (password !== passwordR) {
+    errPasswordR = "Пароли не одинаковы";
+  }
+
+document.getElementById('errPasswordR').innerHTML = errPasswordR;
 }
--- reg.php
+++ reg.php
@@ -1,10 +1,10 @@
-<table border="0">
+<table border="1">
   <tr>
     <td colspan="3"><h1>Регистрация</h1></td>
   </tr>
   <tr>
-    <td>Введите логин</td>
-    <td><input type="text" id="login" onblur="loginScan()"></td>
+    <td width="170">Введите логин</td>
+    <td width="200"><input type="text" id="login" onblur="loginScan()"></td>
     <td width="300"><span id="errLogin"></span></td>
   </tr>
   <tr>
@@ -14,17 +14,18 @@
   </tr>
   <tr>
     <td>Введите Имя</td>
-    <td><input type="text" id="name"></td>
+    <td><input type="text" id="name" onblur="nameScan()"></td>
     <td><span id="errName"></span></td>
   </tr>
   <tr>
     <td>Введите пароль</td>
-    <td colspan="2"><input type="password" id="passw"></td>
+    <td><input type="password" id="password" onblur="passwordScan()"></td>
+    <td><span id="errPassword"></span></td>
   </tr>
   <tr>
     <td>Повторите пароль</td>
-    <td><input type="password" id="passwR"></td>
-    <td><span id="errPassw"></span></td>
+    <td><input type="password" id="passwordR" onblur="passwordRScan()"></td>
+    <td><span id="errPasswordR"></span></td>
   </tr>
   <tr>
     <td colspan="3" height="40"><input type="submit" id="submit" value="Зарегистрироваться" disabled></td>
