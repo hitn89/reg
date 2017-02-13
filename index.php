48e0ab9f99780410ce69c1e40d118ea449dce542
hitn89 <hitn89@gmail.com>
2017-02-13 14:48:10 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <head>
     <meta charset="utf-8">
     <title>Сайт</title>
+    <link rel="stylesheet" type="text/css" media="all" href="img/style.css" />
     <script src="http://yandex.st/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
     <script src="js/script.js" type="text/javascript"></script>
   </head>
